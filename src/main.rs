e8e461b8cf631b12a48b6ccd0aeab59fb654b892
Shimaa Ameen <shemo.adam2016@gmail.com>
2026-02-12 16:10:47 +0300

    a lot more code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let day: i32= 5;
+    match day{
+        1 => println!("sunday"),
+        2 => println!("monday"),
+        3=>println!("tuesday"),
+        4=>println!("wednesday"),
+        5=>println!("thursday"),
+        6=>println!("friday"),
+        7=>println!("saturday"),
+        _=>println!("like thats a real day")
+    }
+    let weekend: bool = day>5;
+    if weekend == true {
+        println!("weekend,yes");
+    }
+    else {
+        println!("weekend no");
+    }
 }
